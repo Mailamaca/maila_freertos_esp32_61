849435fd29c698ed1ccd439699e8775248973d74
Paolo Tomasin <pooltomasin@hotmail.com>
2020-12-04 16:39:18 +0100

    #feat: add maila_msg Esp32Data

--- app.c
+++ app.c
@@ -3,7 +3,8 @@
 
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
-#include <std_msgs/msg/int32.h>
+//#include <std_msgs/msg/int32.h>
+#include <maila_msgs/msg/esp32_data.h>
 
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
@@ -17,14 +18,16 @@
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+//std_msgs__msg__Int32 msg;
+
+maila_msgs__msg__Esp32Data mailamsg;
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
 	RCLC_UNUSED(last_call_time);
 	if (timer != NULL) {
-		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-		msg.data++;
+		RCSOFTCHECK(rcl_publish(&publisher, &mailamsg, NULL));
+		mailamsg.stamp.time++;
 	}
 }
 
@@ -44,7 +47,7 @@ void appMain(void * arg)
 	RCCHECK(rclc_publisher_init_default(
 		&publisher,
 		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		ROSIDL_GET_MSG_TYPE_SUPPORT(maila_msgs, msg, Esp32Data),
 		"maila_freertos_esp32_61"));
 
 	// create timer,
@@ -61,7 +64,8 @@ void appMain(void * arg)
 	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-	msg.data = 0;
+	mailamsg.stamp.time = 0;
+	mailamsg.stamp.nanosec = 0;
 
 	while(1){
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
